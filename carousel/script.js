a31fbc0601e3e0532b4acff3e5811a732c3d1529
Dima Kurbatov <dskurbatov13@gmail.com>
2018-06-06 16:33:38 -0700

    add radio buttons

--- carousel/script.js
+++ carousel/script.js
@@ -5,13 +5,13 @@
 
   class CarouselBox extends HTMLElement {
     static get observedAttributes(){
-      return ['cells']
+      return ['filter']
     }
 
     constructor(){
       super()
-      this._users = []
       this._cells = null
+      this._filter = null
       this.shadow = this.attachShadow({mode: 'open'})
       const tmp = document.importNode(thisDoc.querySelector('template').content, true)
       this.shadow.appendChild(tmp)
@@ -20,64 +20,125 @@
       buttons.forEach(button => {
         button.addEventListener('click', onClick(this.shadow.querySelector('.carousel')))
       })
+
+      const radioButtons = this.shadow.querySelectorAll('input[name="all-online-offline"]')
+      radioButtons.forEach(radio => {
+        radio.addEventListener('change', onChange(this))
+      })
     }
 
-    get cells(){
-      return this._cells
+    get filter(){
+      return this._filter
     }
 
-    set cells(value){
-      this.setAttribute('cells', value)
+    set filter(value){
+      this.setAttribute('filter', value)
     }
 
     attributeChangedCallback(name, oldValue, newValue){
-      const value = parseInt(newValue)
-      
       if(!carousel){
         carousel = this.shadow.querySelector('.carousel')
       }
+
+      if(!this._cells){
+        this._cells = document.querySelectorAll('.carousel-cell')
+      }
       
       switch(name){
-        case 'cells':
-          this.changeCarousel(value)
-          this._cells = value
-          break;
+        case 'filter':
+          this._filter = newValue
+          this.changeCarousel()
       }
     }
 
-    changeCarousel(newValue) {
+    changeCarousel() {
       const width = carousel.offsetWidth
-      const cells = document.querySelectorAll('.carousel-cell')
+      const cellsNumber = countCells(this._filter, this._cells)
       
-      theta = 360 / newValue
-      radius = Math.round((width / 2) / Math.tan(Math.PI / newValue))
-
-      for(let i = 0, len = cells.length; i < len; i++){
-        cells[i].style.opacity = 1
-        cells[i].style.transform = `rotateY(${theta * i}deg) translateZ(${radius}px)` 
+      theta = 360 / cellsNumber
+      radius = Math.round((width / 2) / Math.tan(Math.PI / cellsNumber)) || 1
+      
+      if(this._filter === 'All'){
+        filterAll(this._cells, theta, radius)
+      } else if(this._filter === 'Offline') {
+        filterOffline(this._cells, theta, radius)
+      } else {
+        filterOnline(this._cells, theta, radius)
       }
       rotateCarousel(carousel)
     }
 
     connectedCallback(){
-      const tmp = document.querySelector('#carousel-cell-template')
-      const fragment = new DocumentFragment()
-      const button = document.createElement('button')
-      button.textContent = 'Follow Me'
-      let clone
-      
-      init().then(usersInfo => {
-        this._users = usersInfo
-        this._users.map(({ display_name, logo, stream }) => {
-          clone = document.importNode(tmp.content, true)
-          clone.querySelector('img').src = logo
-          clone.querySelector('h2').textContent = display_name
-          clone.querySelector('p').textContent = (stream) ? `${stream.game} ${stream.status}` : 'Offline'
-          fragment.appendChild(clone)
-        })
+      init().then(fragment => {
         this.appendChild(fragment)
-        this.setAttribute('cells', usersInfo.length)
+        this.setAttribute('filter', 'All')
       })
+    }
+  }
+
+  function filterAll(cells, theta, radius){
+    for(let i = 0, len = cells.length; i < len; i++){
+      cells[i].style.opacity = 1
+      cells[i].style.transform = `rotateY(${theta * i}deg) translateZ(${radius}px)` 
+    }
+  }
+
+  function filterOffline(cells, theta, radius){
+    for(let i = 0, len = cells.length; i < len; i++){
+      if(cells[i].querySelector('p').textContent !== 'Offline'){
+        cells[i].style.opacity = 0
+        cells[i].style.transform = 'none'
+      } else {
+        cells[i].style.opacity = 1
+        cells[i].style.transform = `rotateY(${theta * i}deg) translateZ(${radius}px)`
+      }
+    }
+  }
+
+  function filterOnline(cells, theta, radius){
+    for(let i = 0, len = cells.length; i < len; i++){
+      if(cells[i].querySelector('p').textContent === 'Offline'){
+        cells[i].style.opacity = 0
+        cells[i].style.transform = 'none'
+      } else {
+        cells[i].style.opacity = 1
+        cells[i].style.transform = `rotateY(${theta * i}deg) translateZ(${radius}px)`
+      }
+    }
+  }
+
+  function countCells(filter, cells){
+    let count = 0;
+    switch(filter){
+      case 'All':
+        return cells.length
+        break;
+      case 'Offline':
+        cells.forEach(cell => {
+          if(offline(cell)){
+            count++
+          }
+        })
+        return count
+      case 'Online':
+        cells.forEach(cell => {
+          if(!offline(cell)){
+            count++
+          }
+        })
+        return count
+      default:
+        return cells.length
+    }
+  }
+
+  function offline(cell){
+    return cell.querySelector('p').textContent === 'Offline'
+  }
+
+  function onChange(carousel){
+    return function(e){
+      carousel.setAttribute('filter', this.value)
     }
   }
 
@@ -97,6 +158,7 @@
 
   function rotateCarousel(carousel){
     let angel = theta * currentIndex * -1
+    console.log(angel)
     carousel.style.transform = `translateZ(-${radius}px) rotateY(${angel}deg)`
   }
 
@@ -118,17 +180,21 @@
   }
 
   async function init(){
-    const usersInfo = []
+    const tmp = document.querySelector('#carousel-cell-template')
+    const fragment = new DocumentFragment()
     const [users, streams] = await getData()
+    let clone, stream, user
   
     for(let i = 0, len = userNames.length; i < len; i++){
-      usersInfo.push({
-        display_name: users[i].display_name,
-        logo: users[i].logo,
-        stream: getStreamInfo(streams[i].stream)
-      })
+      stream = streams[i].stream, user = users[i]
+      
+      clone = document.importNode(tmp.content, true)
+      clone.querySelector('img').src = user.logo
+      clone.querySelector('h2').textContent = user.display_name
+      clone.querySelector('p').textContent = (stream) ? `${stream.game} ${stream.channel.status}` : 'Offline'
+      fragment.appendChild(clone)
     }
-    return usersInfo
+    return fragment
   }
 
   function getStreamInfo(stream) {
